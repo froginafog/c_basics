ee246bdf576c31d3f9b60101138e4cc0c6ec24c4
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-17 16:58:45 -0400

    Update main.c

--- c_basics/0057/main.c
+++ c_basics/0057/main.c
@@ -4,19 +4,19 @@
 
 int main()
 {
-	int N;  //N = number of elements
+    int N;  //N = number of elements
     char message[] = "Hello Frog";
 
     N = strlen(message);  //number of elements in "message"
 
-	char * copy_message = malloc((N + 1) * sizeof(char));  //allocate memory for N characters
+    char * copy_message = malloc((N + 1) * sizeof(char));  //allocate memory for N characters
                                                            //+1 for '\0'
     strcpy(copy_message, message);  //copy "message" into "copy_message"
 
-	printf("copy_message: %s\n", copy_message);
-	free(copy_message);
+    printf("copy_message: %s\n", copy_message);
+    free(copy_message);
 
-	return 0;
+    return 0;
 }
 
 /*
