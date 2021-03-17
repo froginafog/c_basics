da4e2d11d603a16445b32a024a3e159cf92b4fda
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-17 17:01:03 -0400

    Update main.c

--- c_basics/0058/main.c
+++ c_basics/0058/main.c
@@ -4,23 +4,23 @@
 
 int main()
 {
-	int N1;
-	int N2;
+    int N1;
+    int N2;
     char message1[] = "Hello Frog";
     char message2[] = " Bye Frog";
 
     N1 = strlen(message1);  //number of elements in "message1"
     N2 = strlen(message2);  //number of elements in "message2"
 
-	char * total_message = malloc((N1 + N2 + 1) * sizeof(char));
+    char * total_message = malloc((N1 + N2 + 1) * sizeof(char));
 
     strcpy(total_message, message1); //copy message1 into total_message
     strcat(total_message, message2); //append (concatenate) message2 to the end of message1
 
-	printf("total_message: %s\n", total_message);
-	free(total_message);
+    printf("total_message: %s\n", total_message);
+    free(total_message);
 
-	return 0;
+    return 0;
 }
 
 /*
