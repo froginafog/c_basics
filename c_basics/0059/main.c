3090352e52a61953d7968a61af25fc4ba7934ec9
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-17 17:01:47 -0400

    Update main.c

--- c_basics/0059/main.c
+++ c_basics/0059/main.c
@@ -4,27 +4,27 @@
 
 int main()
 {
-	int N1;
-	int N2;
+    int N1;
+    int N2;
     char message1[] = "Hello Frog";
     char message2[] = "Bye Frog Have A Nice Day";
 
     N1 = strlen(message1);  //number of elements in "message1"
     N2 = strlen(message2);  //number of elements in "message2"
 
-	char * copy_message;
+    char * copy_message;
 
-	copy_message = malloc((N1 + 1) * sizeof(char));  //allocate memory for storing N1
+    copy_message = malloc((N1 + 1) * sizeof(char));  //allocate memory for storing N1
     strcpy(copy_message, message1); //copy message1 into copy_message
-	printf("copy_message: %s\n", copy_message);
+    printf("copy_message: %s\n", copy_message);
 
-	realloc(copy_message, (N2 + 1) * sizeof(char)); //reallocate memory for storing N2
+    realloc(copy_message, (N2 + 1) * sizeof(char)); //reallocate memory for storing N2
     strcpy(copy_message, message2); //copy message2 into copy_message
     printf("copy_message: %s\n", copy_message);
 
     free(copy_message);
 
-	return 0;
+    return 0;
 }
 
 /*
