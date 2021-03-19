8a027a74ea617285d0ea9dacf487cd1a6f62c334
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-19 14:19:10 -0400

    Update main.c

--- c_basics/0002/main.c
+++ c_basics/0002/main.c
@@ -7,16 +7,16 @@ int main()
 	char ch3;  //declare a variable of the type char, the name of the variable is ch3
 	char ch4;  //declare a variable of the type char, the name of the variable is ch4
 	
-	ch1 = 'f';    
+	ch1 = 'f';  //assign the character 'f' to ch1
 	ch2 = 'r';
 	ch3 = 'o';
 	ch4 = 'g';
 	//single quotes are for storing 1 character at a time
 	
-	printf("%c", ch1);
-	printf("%c", ch2);
-	printf("%c", ch3);
-	printf("%c", ch4);
+	printf("%c", ch1);  //print ch1
+	printf("%c", ch2);  //print ch2
+	printf("%c", ch3);  //print ch3
+	printf("%c", ch4);  //print ch4
 	printf("\n");
 	printf("----------1---------\n");
 	printf("%c%c%c%c", ch1, ch2, ch3, ch4);
