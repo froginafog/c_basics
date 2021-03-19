c8ebd9068ce50881ba0f588f8cb2407739e31a7f
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-19 11:25:12 -0400

    Add files via upload

---
+++ c_basics/0065/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	char * s = (char*)malloc(sizeof(char));
+
+	printf("sizeof(char): %d\n", sizeof(char));
+	printf("sizeof(s): %d\n", sizeof(s));
+
+	char ch;
+	for(ch = 'A', i = 0; ch <= 'Z'; ch++, i++)
+	{
+		s[i] = ch;
+	}
+	s[i] = '\0';
+
+
+	printf("s: ");
+	i = 0;
+	while(s[i] != '\0')
+	{
+		printf("%c", s[i]);
+		i++;
+	}
+
+	printf("\n");
+	free(s);
+	printf("s: %s\n", s);
+
+	return 0;
+}
+
+/*
+sizeof(char): 1
+sizeof(s): 8
+s: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+s:
+*/
