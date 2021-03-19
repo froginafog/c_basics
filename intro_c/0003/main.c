5ecfa8f9bf3d9be8982679a78b4bb82e336a4323
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-19 16:32:23 -0400

    Add files via upload

---
+++ intro_c/0003/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello Frog \n");  //print "Hello Frog" to the screen and go to the next line
+    printf("Bye Frog \n");  //print "Bye Frog" to the screen and go to the next line
+
+    return 0;
+}
+
+//The output of this program on the screen is:
+/*
+Hello Frog
+Bye Frog
+*/
---
+++ intro_c/0005/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("-----%s-----", "Hello Frog"); //print "-----Hello Frog-----" to the screen
+    //'%' is a formatter
+    //'s' means string
+    printf("\n"); //go to the next line
+
+    return 0;
+}
+
+
+/*
+-----Hello Frog-----
+*/
