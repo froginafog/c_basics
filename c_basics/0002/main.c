491dd4fd5d0eaa1b9e903db5ded162884cdd54c3
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-19 14:20:21 -0400

    Update main.c

--- c_basics/0002/main.c
+++ c_basics/0002/main.c
@@ -16,7 +16,7 @@ int main()
 	printf("%c", ch1);  //print the content stored in ch1 to the screen
 	printf("%c", ch2);  //print the content stored in ch2 to the screen
 	printf("%c", ch3);  //print the content stored in ch2 to the screen
-	printf("%c", ch4);  //pprint the content stored in ch2 to the screen
+	printf("%c", ch4);  //print the content stored in ch2 to the screen
 	printf("\n");
 	printf("----------1---------\n");
 	printf("%c%c%c%c", ch1, ch2, ch3, ch4);
