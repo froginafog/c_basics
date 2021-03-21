065a49092f6b992687ffa0fb50bfbb9d4a78d6fa
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-21 16:50:52 -0400

    Update main.c

--- basics_0002/0016/main.c
+++ basics_0002/0016/main.c
@@ -4,9 +4,9 @@ int main()
 {
 	int a[] = {10, 20, 30, 40, 50};
 	int num_elements = 5;
-    int i;
+    	int i;
 
-    //loop through each elements of the array
+   	 //loop through each elements of the array
 	for(i = 0; i < num_elements; i++)
 	{
         printf("a[%d]: %d\n", i, a[i]);
