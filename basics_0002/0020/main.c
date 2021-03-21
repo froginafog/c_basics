8720be4585170202add699aaff95c8d073b19bef
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-21 16:53:05 -0400

    Update main.c

--- basics_0002/0020/main.c
+++ basics_0002/0020/main.c
@@ -8,24 +8,24 @@ int main()
 
 	switch(choice)
 	{
-        case 'A':  //if(choice == 'A')
-            printf("The choice is menu A.\n");
-            printf("It includes a drink, some fries, and a burger\n");
-            break;
-        case 'B':  //else if(choice == 'B')
-            printf("The choice is menu B.\n");
-            printf("It includes a drink, some fries, a burger, and a soup.\n");
-            break;
-        case 'C':  //else if(choice == 'C')
-            printf("The choice is menu C.\n");
-            printf("It includes a drink, some fries, a burger, a soup, and a salad.\n");
-            break;
-        case 'D':  //else if(choice == 'D')
-            printf("The choice is menu D.\n");
-            printf("It includes a drink, some fries, a burger, a soup, a salad, and a desert.");
-            break;
-        default:  //else
-            printf("invalid choice");
+		case 'A':  //if(choice == 'A')
+		    printf("The choice is menu A.\n");
+		    printf("It includes a drink, some fries, and a burger\n");
+		    break;
+		case 'B':  //else if(choice == 'B')
+		    printf("The choice is menu B.\n");
+		    printf("It includes a drink, some fries, a burger, and a soup.\n");
+		    break;
+		case 'C':  //else if(choice == 'C')
+		    printf("The choice is menu C.\n");
+		    printf("It includes a drink, some fries, a burger, a soup, and a salad.\n");
+		    break;
+		case 'D':  //else if(choice == 'D')
+		    printf("The choice is menu D.\n");
+		    printf("It includes a drink, some fries, a burger, a soup, a salad, and a desert.");
+		    break;
+		default:  //else
+		    printf("invalid choice");
 	}
 
 	return 0;
