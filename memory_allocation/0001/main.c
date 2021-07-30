1cda93faf6894242151a284d183c1e4af8e5b6f0
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-07-30 05:35:56 -0400

    Add files via upload

---
+++ memory_allocation/0001/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;  //N = number of elements
+
+	printf("Enter the number of elements you wish to enter: ");
+	scanf("%d", &N);
+
+	int * a = (int*)malloc(N * sizeof(int));
+	//Allocate memory for N intergers for pointer "a".
+	//"a" can now be seen as an array
+
+	//check if memory is allocated
+	if(a == NULL)
+	{
+		printf("error (failed to allocate memeory)\n");
+		exit(0);
+	}
+
+	int i;
+
+	for(i = 0; i < N; i++)
+	{
+		printf("Enter an element for a[%d]: ", i);
+		scanf("%d", &a[i]);
+	}
+
+	int sum = 0;
+
+	printf("a: ");
+	for(i = 0; i < N; i++)
+	{
+		printf("%d  ", a[i]);
+		sum = sum + a[i];
+	}
+	printf("\n");
+
+	printf("The sum of the elements of a: %d\n", sum);
+
+	free(a);  //free the memory allocated to "a"
+	printf("free(a) is called\n");
+
+    printf("a: ");
+	for(i = 0; i < N; i++)
+	{
+		printf("%d  ", a[i]);
+		sum = sum + a[i];
+	}
+	printf("\n");
+
+	return 0;
+}
+
+/*
+Enter the number of elements you wish to enter: 5
+Enter an element for a[0]: 10
+Enter an element for a[1]: 20
+Enter an element for a[2]: 30
+Enter an element for a[3]: 40
+Enter an element for a[4]: 50
+a: 10  20  30  40  50
+The sum of the elements of a: 150
+free(a) is called
+a: 0  0  -1938317296  22081  50
+*/
